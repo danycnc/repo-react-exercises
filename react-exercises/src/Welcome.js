18fc43467d89076560d2a3665a223cc532041972
Daniele Canale <danycnc95@gmail.com>
2022-05-10 15:29:56 +0200

    svolto cond-rend-04

--- react-exercises/src/Welcome.js
+++ react-exercises/src/Welcome.js
@@ -6,10 +6,11 @@ class Welcome extends React.Component {
     return (
       <div>
         <p>Welcome, {this.props.name || 'Daniele'}!</p>
+        {console.log(this.props.name)}
 
-        {this.props.age > 18 && this.props.age < 65 && (
-          <Age age={this.props.age} />
-        )}
+        {this.props.name === 'Jonh' &&
+          this.props.age > 18 &&
+          this.props.age < 65 && <Age age={this.props.age} />}
       </div>
     );
   }
