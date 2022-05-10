0ad866c11e551bf98da52b29445fd526d239ee7e
Daniele Canale <danycnc95@gmail.com>
2022-05-10 14:38:47 +0200

    svolto props-01

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,11 +1,13 @@
 import { Component } from 'react';
 import Hello from './Hello';
+import Welcome from './Welcome';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Hello />
+        <Welcome name={'Daniele'} />
       </div>
     );
   }
