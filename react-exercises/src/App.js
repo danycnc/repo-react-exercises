6e17eaebcd930f6cd74d277f5687f2f97e39f11b
Daniele Canale <danycnc95@gmail.com>
2022-05-16 12:43:00 +0200

    svolto lists-03

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -5,6 +5,7 @@ import Counter from './Counter';
 import Hello from './Hello';
 import InteractiveWelcome from './InteractiveWelcome';
 import Login from './Login';
+import TodoList from './TodoList';
 import UncontrolledLogin from './UncontrolledLogin';
 import Welcome from './Welcome';
 
@@ -24,6 +25,7 @@ class App extends Component {
         <InteractiveWelcome />
         <Login currentState={onLogin} />
         <UncontrolledLogin currentState={onLogin} />
+        <TodoList />
       </div>
     );
   }
---
+++ react-exercises/src/TodoList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+class TodoList extends React.Component {
+  state = {
+    newTodo: '',
+    items: ['Buy milk', 'Kill the cat'],
+  };
+
+  handleInput = (event) => {
+    const newTodo = event.target.value;
+
+    this.setState({ newTodo: newTodo });
+  };
+
+  handleAddTodo = () => {
+    const { newTodo, items } = this.state;
+    items.push(newTodo);
+    this.setState({ items: items });
+  };
+
+  render() {
+    const items = this.state.items.map((name, index) => (
+      <li key={name + index}>{name}</li>
+    ));
+    return (
+      <div>
+        <h3>TodoList</h3>
+        <input
+          name='newTodo'
+          value={this.state.newTodo}
+          onChange={this.handleInput}></input>
+        <button onClick={this.handleAddTodo}>Add</button>
+        <ul>{items}</ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
