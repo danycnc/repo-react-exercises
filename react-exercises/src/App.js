1afb44c6608b014e4655aad4bbd42027908828a0
Daniele Canale <danycnc95@gmail.com>
2022-05-27 16:40:18 +0200

    svolto useContext

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -3,42 +3,40 @@ import ClickCounter from './ClickCounter';
 import ClickTracker from './ClickTracker';
 import Container from './Container';
 import Counter from './Counter';
+import DisplayLanguage from './DisplayLanguage';
 import Hello from './Hello';
 import InteractiveWelcome from './InteractiveWelcome';
 import Login from './Login';
 import TodoList from './TodoList';
 import UncontrolledLogin from './UncontrolledLogin';
 import Welcome from './Welcome';
+import { LanguageContext } from './LanguageContext';
 
 const onLogin = (state) => {
   return console.log(state);
 };
 
 class App extends Component {
+  state = {
+    language: 'en',
+  };
+
+  handleLanguage = (event) => {
+    this.setState({
+      language: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
-        <TodoList
-          render={(items, reset) => {
-            items.map((todo, index) => (
-              <li key={todo + index}>
-                {todo}
-                <button onClick={() => reset(index)}>Delete</button>
-              </li>
-            ));
-            return (
-              <div>
-                <h3>TodoList</h3>
-                <input
-                  name='newTodo'
-                  //value={this.state.newTodo}
-                  onChange={this.handleInput}></input>
-                <button onClick={this.handleAddTodo}>Add</button>
-                <button onClick={this.handleResetAll}>Delete All</button>
-                <ul>{items}</ul>
-              </div>
-            );
-          }}></TodoList>
+        <select value={this.state.language} onChange={this.handleLanguage}>
+          <option value='en'>English</option>
+          <option value='it'>Italiano</option>
+        </select>
+        <LanguageContext.Provider value={this.state.language}>
+          <DisplayLanguage />
+        </LanguageContext.Provider>
       </div>
     );
   }
