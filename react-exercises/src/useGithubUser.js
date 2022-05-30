d8e98661ce98f0f093eb7dafdcb9c0b85f9418b0
Daniele Canale <danycnc95@gmail.com>
2022-05-30 13:42:47 +0200

    svolto swr-03

--- react-exercises/src/useGithubUser.js
+++ react-exercises/src/useGithubUser.js
@@ -3,12 +3,21 @@ import useSWR from 'swr';
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 const useGithubUser = (username) => {
-  const { data, error } = useSWR(
+  const { data, error, mutate } = useSWR(
     username ? `https://api.github.com/users/${username}` : null,
     fetcher
   );
 
-  return { loading: !data && !error, error, data };
+  const handleRefreshUser = () => {
+    mutate();
+  };
+
+  return {
+    loading: !data && !error,
+    error,
+    data,
+    onRefresh: handleRefreshUser,
+  };
 };
 
 export default useGithubUser;
